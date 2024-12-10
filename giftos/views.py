1bb3068011db572eeef1c557ca3daada2b005fce
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-12-10 22:10:41 +0000

    update

--- giftos/views.py
+++ giftos/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render, redirect
 
 def create_superuser(request):
     password = "testing321"
-    if User.objects.filter(email='admin@giftos.com').exists():
+    if User.objects.filter(email='admin@devgearhub.com').exists():
         return redirect("homepage")
-    User.objects.create_superuser(username="admin", email='admin@giftos.com', password=password)
-    user = User.objects.get(email='admin@giftos.com')
+    User.objects.create_superuser(username="admin", email='admin@devgearhub.com', password=password)
+    user = User.objects.get(email='admin@devgearhub.com')
     user.set_password(password)
     user.save()
     return redirect("homepage")
